e45d035974e7967867eaaa830813e27bdd0e2230
FrancoiseHayek <hayekfreelancing@gmail.com>
2025-05-24 19:57:54 -0400

    Channels for communicating between goroutines

---
+++ channels.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	c := make(chan int)
+
+	go func() {
+
+		sum := 0
+		for i := 0; i <= 100; i++ {
+			sum += i
+		}
+
+		c <- sum
+	}()
+
+	output := <-c
+
+	fmt.Println("Output:", output)
+
+}
